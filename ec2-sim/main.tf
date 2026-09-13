f28dded8a7e5a3c151db2ba32a2b362312a8a317
AymaneFirmli-eng <aymane.byzm@gmail.com>
2026-09-13 23:40:10 +0200

    Add AWS provider and EC2 instance configuration

--- ec2-sim/main.tf
+++ ec2-sim/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+}
 
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-085925f297f89fce1"
+  instance_type = "t2.micro"
+}
