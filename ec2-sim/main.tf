4f1795bd472d3ba00c13a040f795e775a628ece5
AymaneFirmli-eng <aymane.byzm@gmail.com>
2026-09-15 09:37:18 +0200

    Define variables for AWS configuration
    Added variables for AWS region, instance type, AMI ID, and environment name.

--- ec2-sim/main.tf
+++ ec2-sim/main.tf
@@ -1,13 +1,23 @@
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
+variable "aws_region" {
+  description = "La région AWS ciblée"
+  type        = string
+  default     = "us-east-1"
 }
 
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-085925f297f89fce1"
-  instance_type = "t2.micro"
+variable "instance_type" {
+  description = "Le type d'instance EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "L'ID de l'AMI pour l'instance"
+  type        = string
+  default     = "ami-085925f297f89fce1"
+}
+
+variable "environment_name" {
+  description = "Nom de l'environnement pour les tags"
+  type        = string
+  default     = "Sandbox-LocalStack"
 }
